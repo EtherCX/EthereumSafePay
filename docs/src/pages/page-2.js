21fee379be3193750688c9a7e7f56d9e7bf28df2
Dennis E <git@destenson.digital>
2017-08-11 01:21:19 -0700

    adds stub site

---
+++ docs/src/pages/page-2.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+var Sigh = () => {
+  <p>Sigh!</p>
+};
+
+const SecondPage = () =>
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to page 2, ok?</p><Sigh />
+    <p>Got it</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+
+export default SecondPage
